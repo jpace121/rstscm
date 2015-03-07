51000b058cfb3f771916d6de84957701d906fbc0
Jimmy Pace <pacejimmy@yahoo.com>
2015-03-07 17:50:24 -0500

    Make non-functionized tokenize compile.

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,22 @@
+extern crate regex;
+use regex::Regex;
+
 fn main() {
-    let test = "(abc)".to_string();
-    let token = tokenize(&test);
+    let re1 = Regex::new(r"\(").unwrap();
+    let re2 = Regex::new(r"\)").unwrap();
+
+    let test = "(abc)";
     
+    let mut after = re1.replace_all(test,"( ");
+    after = re2.replace_all(after.as_slice(), " )");
+
+    println!("{}",after)
 }
 
+/*
 fn tokenize<'s>(chars: &'s String) -> Vec<&'s str> {
     let with_space: &'s String = chars.replace("(", "( ");
     let vected: Vec<&'s str>  = with_space.as_slice().split(' ').collect();
     return vected
 }
+*/
