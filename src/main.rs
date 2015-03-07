4de8d7231d40250770862a102f55c53eacd8b9be
Jimmy Pace <pacejimmy@yahoo.com>
2015-03-07 18:37:37 -0500

    Tokenize compiles!
    Proably the least idiomatic thing ever, but it compiles!

--- src/main.rs
+++ src/main.rs
@@ -4,15 +4,26 @@ use regex::Regex;
 fn main() {
 
     let test = "(abc)".to_string();
-    let tokened: String = tokenize(&test);
+    let tokened: Vec<String> = tokenize(&test);
     
-    println!("{}",tokened)
+    println!("{:?}",tokened)
 }
 
-fn tokenize(input: &String) -> String{
+fn tokenize<'s>(input: &'s String) -> Vec<String>{
+    let mut tokens = vec![];
     
     let re1 = Regex::new(r"\(").unwrap();
     let re2 = Regex::new(r"\)").unwrap();
-    let after = re1.replace_all(input,"( ");
-    re2.replace_all(after.as_slice(), " )")
+    let mut after = re1.replace_all(input,"( ");
+    after = re2.replace_all(after.as_slice(), " )");
+
+    let after_sliced: &str = after.as_slice();
+    let mut split = after_sliced.split_str(" ");
+
+    //I bet there is a better way to do this, but I keep getting lifetime errors.
+    for token in split {
+        tokens.push(token.to_string());
+    }
+
+    tokens
 }
