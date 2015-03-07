9aed30909bc8f181cf3d8fb3988bf2a6ba77a4ab
Jimmy Pace <pacejimmy@yahoo.com>
2015-03-07 18:03:08 -0500

    Tokenizes add spaces.

--- src/main.rs
+++ src/main.rs
@@ -2,21 +2,17 @@ extern crate regex;
 use regex::Regex;
 
 fn main() {
+
+    let test = "(abc)".to_string();
+    let tokened: String = tokenize(&test);
+    
+    println!("{}",tokened)
+}
+
+fn tokenize(input: &String) -> String{
+    
     let re1 = Regex::new(r"\(").unwrap();
     let re2 = Regex::new(r"\)").unwrap();
-
-    let test = "(abc)";
-    
-    let mut after = re1.replace_all(test,"( ");
-    after = re2.replace_all(after.as_slice(), " )");
-
-    println!("{}",after)
+    let after = re1.replace_all(input,"( ");
+    re2.replace_all(after.as_slice(), " )")
 }
-
-/*
-fn tokenize<'s>(chars: &'s String) -> Vec<&'s str> {
-    let with_space: &'s String = chars.replace("(", "( ");
-    let vected: Vec<&'s str>  = with_space.as_slice().split(' ').collect();
-    return vected
-}
-*/
