c7c4372292d5e6c52e4fe234f1493571ee299ec4
Jimmy Pace <pacejimmy@yahoo.com>
2015-03-09 22:03:07 -0400

    Add tests for tokenize.

--- src/main.rs
+++ src/main.rs
@@ -11,26 +11,32 @@ enum schmAtom {
 
 fn main() {
 
-    let test = "(abc)".to_string();
-    let tokened = tokenize(&test);
-    
-    println!("{:?}",tokened)
 }
 
-fn tokenize(input: & String) -> Vec<String>{
-    //let mut tokens = vec![];
+fn tokenize(input: String) -> Vec<String>{
     
     let re1 = Regex::new(r"\(").unwrap();
     let re2 = Regex::new(r"\)").unwrap();
-    let mut after: String = re1.replace_all(input,"( ");
+    let mut after: String = re1.replace_all(&input,"( ");
     after = re2.replace_all(&after, " )");
 
     let split = after.split(" "); //split is an iterator
     split.map(|x|{x.to_string()}).collect::<Vec<String>>()
     //^to `.to_string` seems kind of hackish...
+    //BUT it actaully just forces the copy and move of the string
 }
 
 fn read_from_tokens(){
 }
+
+#[cfg(test)]
+
+#[test]
+fn test_tokenize() {
+    let test_input: String  = "(a b c)".to_string();
+    assert_eq!(tokenize(test_input), 
+               vec!("(","a","b","c", ")"));
+}
+
 
 
